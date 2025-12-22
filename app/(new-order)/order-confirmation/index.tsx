19d4d763c0ac1b98ffca35cc151767b831c5d7b6
Santiago Quirumbay <santiagoquirumbay10@gmail.com>
2025-12-22 07:59:08 -0500

    Fix order confirmation screen

--- app/(new-order)/order-confirmation/index.tsx
+++ app/(new-order)/order-confirmation/index.tsx
@@ -8,6 +8,7 @@ import { OrderType } from "@/core/orders/enums/order-type.enum";
 import { useOrderStatus } from "@/presentation/orders/hooks/useOrderStatus";
 import { useTranslation } from "@/core/i18n/hooks/useTranslation";
 import { formatCurrency } from "@/core/i18n/utils";
+import { OrderStatus } from "@/core/orders/enums/order-status.enum";
 
 export default function OrderConfirmationScreen() {
   const { t } = useTranslation(["common", "orders"]);
@@ -20,11 +21,11 @@ export default function OrderConfirmationScreen() {
     setActiveOrder(null);
   };
 
+  const { statusText } = useOrderStatus(order?.status || OrderStatus.PENDING);
+
   if (!order) {
     return null;
   }
-
-  const { statusText } = useOrderStatus(order?.status);
 
   return (
     <>
@@ -79,9 +80,7 @@ export default function OrderConfirmationScreen() {
           {order.notes && (
             <ThemedView style={tw` w-full bg-gray-100 p-4 rounded-lg`}>
               <ThemedView style={tw`  bg-transparent gap-2 `}>
-                <ThemedText type="body2">
-                  {t("common:labels.notes")}
-                </ThemedText>
+                <ThemedText type="body2">{t("common:labels.notes")}</ThemedText>
                 <ThemedText type="body1">
                   {order.notes || t("common:labels.noNotes")}
                 </ThemedText>
@@ -98,35 +97,39 @@ export default function OrderConfirmationScreen() {
                   style={tw`flex-row items-center bg-transparent gap-3 `}
                 >
                   <ThemedText type="body1">{item.quantity}</ThemedText>
-                <ThemedText type="body1">{item.product.name}</ThemedText>
+                  <ThemedText type="body1">{item.product.name}</ThemedText>
+                </ThemedView>
+                <ThemedText type="body1">
+                  {formatCurrency(item.amount)}
+                </ThemedText>
               </ThemedView>
-              <ThemedText type="body1">{formatCurrency(item.amount)}</ThemedText>
+            ))}
+            <ThemedView
+              style={tw`flex-row justify-between items-center bg-transparent `}
+            >
+              <ThemedText type="h4">
+                {t("orders:confirmation.total")}
+              </ThemedText>
+              <ThemedText type="body1" style={tw`font-bold`}>
+                {formatCurrency(order.total)}
+              </ThemedText>
             </ThemedView>
-          ))}
-          <ThemedView
-            style={tw`flex-row justify-between items-center bg-transparent `}
-          >
-            <ThemedText type="h4">{t("orders:confirmation.total")}</ThemedText>
-            <ThemedText type="body1" style={tw`font-bold`}>
-              {formatCurrency(order.total)}
-            </ThemedText>
           </ThemedView>
         </ThemedView>
-      </ThemedView>
-      <ThemedView style={tw`w-full flex-row gap-4 justify-center`}>
-        <Button
-          leftIcon="home-outline"
-          label={t("orders:confirmation.goToHome")}
-          onPress={goToHome}
-          variant="outline"
-        ></Button>
-        <Button
-          leftIcon="pencil-outline"
-          label={t("orders:confirmation.editOrder")}
-          onPress={() => router.push("/(order)/[id]")}
-          variant="primary"
-        ></Button>
-      </ThemedView>
+        <ThemedView style={tw`w-full flex-row gap-4 justify-center`}>
+          <Button
+            leftIcon="home-outline"
+            label={t("orders:confirmation.goToHome")}
+            onPress={goToHome}
+            variant="outline"
+          ></Button>
+          <Button
+            leftIcon="pencil-outline"
+            label={t("orders:confirmation.editOrder")}
+            onPress={() => router.push("/(order)/[id]")}
+            variant="primary"
+          ></Button>
+        </ThemedView>
       </ThemedView>
     </>
   );
