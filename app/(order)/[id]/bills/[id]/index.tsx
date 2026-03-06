970d148455c5bc967bd5a6186c8a6dfa6c6624ee
Santiago Quirumbay <santiagoquirumbay10@gmail.com>
2026-03-06 08:02:38 -0500

    Add moneyReceived options

--- app/(order)/[id]/bills/[id]/index.tsx
+++ app/(order)/[id]/bills/[id]/index.tsx
@@ -104,7 +104,8 @@ export default function BillScreen() {
 
   const moneyReceivedOptions = [
     5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95,
-    100,
+    100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170,
+    175, 180, 185, 190, 195, 200,
   ];
   const commissionOptions = [2.4, 4.5, 5.4];
   const date = getFormattedDate(bill.createdAt);
